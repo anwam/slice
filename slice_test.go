8a773758b1fe151fb5a2828e24b3d0d4459ffb35
anonymous <anuwong@dollarsign.co>
2022-03-21 18:10:59 +0700

    docs: add test case for Reduce

--- slice_test.go
+++ slice_test.go
@@ -8,13 +8,13 @@ import (
 )
 
 func Test_Map(t *testing.T) {
-	ast := assert.New(t)
+	assert := assert.New(t)
 	expected := []int{2, 4, 6, 8, 10}
 	result := Map([]int{1, 2, 3, 4, 5}, func(i int) int {
 		i = i * 2
 		return i
 	})
-	ast.Equal(expected, result)
+	assert.Equal(expected, result)
 }
 
 func Test_MapIntToString(t *testing.T) {
@@ -23,5 +23,23 @@ func Test_MapIntToString(t *testing.T) {
 	result := Map([]int{1, 5, 10}, func(i int) string {
 		return strconv.Itoa(i)
 	})
+	assert.Equal(expected, result)
+}
+
+func Test_Reduce(t *testing.T) {
+	assert := assert.New(t)
+	expected := 15
+	result := Reduce([]int{1, 2, 3, 4, 5}, func(acc, i int) int {
+		return acc + i
+	}, 0)
+	assert.Equal(expected, result)
+}
+
+func Test_ReduceIntToString(t *testing.T) {
+	assert := assert.New(t)
+	expected := "12345"
+	result := Reduce([]int{1, 2, 3, 4, 5}, func(acc string, i int) string {
+		return acc + strconv.Itoa(i)
+	}, "")
 	assert.Equal(expected, result)
 }
